154fdc1b557eba64bec08a25150ac3a6c7e1f107
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-18 21:13:20 +0100

    added comments to the code

--- project_3.sh
+++ project_3.sh
@@ -5,10 +5,10 @@
 
 for i in {3..225}; do ping -c 1 -W 1 192.168.1.$i  # to iterate through a network of servers with IP address from 192.168.1.3 to 192.168.1.225
     if [ $? -ne 0 ]; then
-        echo $i >> "/opt/server_list.txt"
+        echo $i >> "/opt/server_list.txt"  # to input the server list into the /opt/server_list.txt file
 
         # email information
-        Recipient="your_email@email.com"
+        Recipient="your_email@email.com"  # input a valid email address
         Subject="Down_server list"
         Body="Find an attachment of the list of down servers in the /opt/server_list.txt file"
 
