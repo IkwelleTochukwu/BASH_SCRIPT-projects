71ffb8ce4f2e6b3456148a869d8bc4ecabfb35bb
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-19 11:54:55 +0100

    script to parse a file and execute command

---
+++ project_6.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Write a shell script Lock all users whose names are in a file called users.txt 
+
+file_name=$1   # assign the the file_name variable to the parameter
+
+# to check if the parameter is used to run the script
+if [ $# -eq 1 ]; then
+    echo "Parsing file now..."
+    if [ -e "$file_name" ]; then
+
+        # to check if the users exists in the system and lock the users 
+        for users in $file_name; do
+            if id "$users" &>/dev/null; then
+                echo "User $users exists!"
+            else 
+                echo "This user $users do not exist"
+            fi
+
+            sudo passwd -l "$users"
+            echo "User $users is locked now"
+        done
+    else
+        echo "Error: This file do not exists."
+    fi
+else
+    echo  "Usauge: $0 <file>"
+fi
+ 
