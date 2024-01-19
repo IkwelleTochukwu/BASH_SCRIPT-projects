d26ca46fedbba56349df29af521ee98a371f450a
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-19 04:47:30 +0100

    added code to lock the users within the range

--- project_5.sh
+++ project_5.sh
@@ -9,9 +9,14 @@ MAX_UID=530
 USERS=$(awk -F':' -v min=$MIN_UID -v max=$MAX_UID '$3 >= min && $3 <= max {print $1}' /etc/passwd)
 
 # Check if users are within the UID range of 500 to 530
-if [ -n "$USER" ]; then
+if [ -n "$USERS" ]; then
     echo "Users with UID between 530 and 500:"
     echo "$USERS"
+    # To lock the users within the range
+    for user in $USERS; do
+        sudo passwd -l "$user"
+        echo "User $user is locked."
+    done
 else
     echo "There are no users within the UID range of 530 to 500"
 fi
