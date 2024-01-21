6145a429715a3548211e5e3dc7eefcea3d44b31c
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-21 15:53:51 +0100

    added the sudo command to elevate permissions

--- project_7.sh
+++ project_7.sh
@@ -14,22 +14,23 @@ if [ -d "$directory_path" ]; then
     cd "$directory_path" || exit
 
     # List contents of the directory
-    List_of_files=$(find . -type f)    # using find command to recursively find files in the directory and sub-dirs
+    List_of_files=$(sudo find . -type f) 2> /dev/null  # using find command to recursively find files in the directory and sub-dirs
 
     # Iterate over the list of files and check if they are a file
     for log_file in $List_of_files; do
         if [ -f "$log_file" ]; then
 
             # use wc command to count the number lines in the file
-            number_of_line_to_keep=1000
-            line_count=$( wc -l < "$log_file" )
+            number_of_lines_to_keep=1000
+            line_count=$( sudo wc -l < "$log_file" )
             # check if the file have at least 1000 lines
             if [ "$line_count" -ge 1000 ]; then
                 # calculate number of lines to remove
-                lines_to_remove=$((line_count - number_of_line_to_keep))
+                lines_to_remove=$((line_count - number_of_lines_to_keep))
                 # Use tail to get the lines to keep, and sed to remove the remaining lines
-                tail -n "$number_of_line_to_keep" "$log_file" > "$log_file.modified"
-                sed -i "1,${lines_to_remove}d" "$log_file.modified"
+                sudo tail -n "$number_of_lines_to_keep" "$log_file" > "$log_file.modified"
+                sudo sed -i "1,${lines_to_remove}d" "$log_file.modified"
+                sudo mv "$log_file.modified" "$log_file"
             fi
         fi
     done
